da6374c11f3b071f4452ba2b9cd629a685bde961
NavdeepBagga <gottarocknow@gmail.com>
2012-10-25 16:34:54 +0200

    userinput

--- register.php
+++ register.php
@@ -2,6 +2,7 @@
 include 'db.php';
 $ldaphost = "localhost";
 $ldapuser = 'cn=admin,dc=XXX,dc=XXX';
+
 $ldappass = 'XXX';
 // Connecting to LDAP
 $ldapconn = ldap_connect($ldaphost)
@@ -13,10 +14,24 @@ ldap_set_option($ldapconn, LDAP_OPT_REFERRALS, 0);
 
 if ($ldapconn) { 
     $ldapbind=ldap_bind($ldapconn, $ldapuser, $ldappass);     
-    $info["cn"] = "pk";
-    $info["sn"] = "Jones";
+    
+    if($ldapbind){ echo "bind successfull"; }
+    else { echo "not bind"; }
+    if($_POST){
+	    
+	    $info["cn"] = "yuvraj";
+    $info["sn"] = "singh";
+    $info["userpassword"] = $_POST['password'];
+$user= $_POST['username'];
+    $info["objectclass"] = "inetOrgPerson";
+    
     // add data to directory
-    $r = ldap_add($ldapconn, "cn=XXX,ou=XXX,dc=XXX,dc=bagga", $info);
+    $r = ldap_add($ldapconn, "uid=$user,ou=People,dc=XXX,dc=XXX", $info);
+    
+    if($r){ echo "ADD successfull"; }
+    else { echo "not ADD "; }
+    }
+    
 }
 else
 {
@@ -24,4 +39,8 @@ echo "ldap is not working";
 }
 
 ?>
-
+<form action="register.php" method="post">
+Username : <input type="text" name="username"/>
+Password : <input type="password" name="password"/>
+<input type="submit" name="login"/>
+</form>
