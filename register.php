f58ee13cfed9d1e6481d359134d38d24da65dfc2
NavdeepBagga <gottarocknow@gmail.com>
2012-10-29 02:04:46 +0100

    validationlink

--- register.php
+++ register.php
@@ -2,47 +2,50 @@
 include 'db.php';
 if ($ldapConn) { 
     $ldapBind=ldap_bind($ldapConn, $ldapLogin, $ldapPass);     
-    
     if($_POST){
-    //mysql insert
-   $insertvalue= mysql_query("INSERT INTO users (firstname, lastname, email, password, gender, dob, phoneno)
-	   VALUES ('$_POST[firstname]', '$_POST[lastname]','$_POST[username]', '$_POST[password]', '$_POST[gender]', '$_POST[birthdate]', '$_POST[phone]')");
-
-   if($insertvalue)
-   {
-	   $result = mysql_query("SELECT id,email,password FROM users");
-
-	   while($row = mysql_fetch_array($result))
-		     {
-			 $userid=    $row['id']; 
-			 $username=    $row['email'];
-			 $userpasswd=    $row['password'];
-			         }
-   }	   
-	   //ldap insert	    
-    $info["uid"] = $userid;
-    $info["sn"] = "singh";
-    $info["userpassword"] = $userpasswd;
-    $info["objectclass"] = "inetOrgPerson";
-    
-    // add data to directory
-     $ldapAdd = ldap_add($ldapConn, "cn=$username,$ldapDomain", $info);
-     
-    if($ldapAdd){
-	    echo "ADD successfull";
+        // generate activation key
+        $activation=md5($_POST['username']);
+        // insert user input values
+        $insertvalue= mysql_query("INSERT INTO users (firstname, lastname, email, password, gender, dob, phoneno, activation)
+        VALUES ('$_POST[firstname]', '$_POST[lastname]','$_POST[username]', '$_POST[password]', '$_POST[gender]', '$_POST[birthdate]', '$_POST[phone]', '$activation' )");
+        if($insertvalue) {
+            $result = mysql_query("SELECT id,email,password,activation FROM users");
+            while($row = mysql_fetch_array($result))
+    	    {
+    		    $userid = $row['id']; 
+    		    $username = $row['email'];
+    		    $userpasswd = $row['password'];
+    		    $activkey = $row['activation'];
+            }
+            // ldap add    
+            $info["uid"] = $userid;
+            $info["sn"] = "singh";
+            $info["userpassword"] = $userpasswd;
+            $info["objectclass"] = "inetOrgPerson";
+            $ldapAdd = ldap_add($ldapConn, "cn=$username,$ldapDomain", $info);
+            // send mail
+            $to      = $_POST['username'];
+            $subject = 'php';
+            $message = 'Please click the link below to activate yourself on using smsapi.http://localhost/api/register.php?email=$to&key=$activkey';
+            $headers = 'From: gottarocknow@gmail.com';
+            mail($to, $subject, $message);
+            echo "Thank you for registering! A confirmation email has been sent to " . $_POST['username'] . " Please click on the Activation Link to Activate your account";
+        }
     }
-    else { 
-	    echo "not ADD "; }
-    }
-    
 }
 else
 {
-echo "ldap is not working";		
+echo "ldap is not connected";		
 }
-
+if(isset($_GET['key']) && ($_GET['email']))
+{
+    $res = mysql_fetch_array(mysql_query("SELECT activation FROM users WHERE email='$_GET[email]'"));
+    if($res['activation'] = $_GET['key']) {
+        echo "Your account is now active ".$_GET['email'];
+    }
+}
 ?>
-<form action="register.php" method="post">
+<form action="register.php" method="post" >
 Firstname : <input type="text" name="firstname"/>
 Lastname : <input type="text" name="lastname"/>
 Email : <input type="text" name="username"/>
