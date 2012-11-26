c6750d995c63130d204d8a448032d4591e9d670a
NavdeepBagga <gottarocknow@gmail.com>
2012-11-27 03:09:33 +0530

    classfile

--- ProcessSms.php
+++ ProcessSms.php
@@ -4,21 +4,45 @@ class Sms_Processor
 {	
     function validNumber()
     {
-        //if($number)//preg_match
+        
     }
-    function authUser($apikey,$Conn,$Domain)
+    function authUser($apikey,$ldapConn,$ldapDomain)
     {
         $searchFor = $apikey;
         $searchField = "sn";
         $getField = array("cn", "uid");
         $filter = "($searchField=$searchFor)";
-        $ldapSearch = ldap_search($Conn, $Domain, $filter, $getField);
-	$info = ldap_get_entries($Conn, $ldapSearch);
-	return $info["count"];
+        $ldapSearch = ldap_search($ldapConn, $ldapDomain, $filter, $getField);
+        $info = ldap_get_entries($ldapConn, $ldapSearch);
+        if($info["count"] == 1) {
+	        for ($x=0; $x<$info["count"]; $x++) {
+                $user = $info[$x]['uid'][0];
+            }
+        return $user;
+	    }
+	    else {
+            print_r("register urself");
+	    }
     }
-    function checkSms()
+    function checkSms($select,$usersTable,$userid,$sendTable,$number,$message)
     {
-        
+        $check = mysql_query("SELECT $select FROM $usersTable WHERE id='$userid'");
+        while($account = mysql_fetch_array($check))
+        {
+            if($account['smsaccount'] == 0)
+            {
+                echo "You are left with empty message account";
+	        }
+	        else {
+                $query="INSERT INTO $sendTable(momt,sender,receiver,msgdata,boxc_id) VALUES ('MT','$userid','$number','$message','mysqlbox')";
+		        mysql_query($query) or die (mysql_error());
+		        $current = $account['smsaccount'] - 1;	
+		        $minus = mysql_query("UPDATE $usersTable SET $select='$current' WHERE id='$userid'");
+                if($query) {
+                    echo "message successfully sent";
+		        }
+		    }
+        }
     }
 }
 
