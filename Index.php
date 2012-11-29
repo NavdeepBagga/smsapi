9dd227a3935c669296140d793790b47d3bc67ff7
NavdeepBagga <gottarocknow@gmail.com>
2012-11-29 11:52:33 +0530

    indexfile

--- Index.php
+++ Index.php
@@ -5,8 +5,9 @@ require_once 'ProcessSms.php';
 
 //Create object of class	
 $Sms_Process_Obj = new Sms_Processor();
+$numValid = $Sms_Process_Obj -> validNumber($_POST['number']);
 $userAuth = $Sms_Process_Obj -> authUser($_POST['api'],$ldapConn,$ldapDomain);
-$sendSms = $Sms_Process_Obj -> checkSms('smsaccount',$usersTable,$userAuth,$sendTable,$_POST['number'],$_POST['message']);
+$sendSms = $Sms_Process_Obj -> checkSms('smsaccount',$usersTable,$userAuth,$sendTable,$numValid,$_POST['message']);
 echo $sendSms;
 
 ?>
